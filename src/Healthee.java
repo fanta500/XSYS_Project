112c34cf007ca4d60f81eff668792476f9e88c56
Alexander Schacht <a.schacht24@gmail.com>
2019-04-25 15:54:53 +0200

    Small changes.

--- src/Healthee.java
+++ src/Healthee.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Healthee {
+    //MISC
+    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 
     //MODULES
     private Settings settings;
@@ -21,7 +23,7 @@ public class Healthee {
         totalIllnessHistory = new TotalIllnessHistory();
 
         window = new JFrame("Healthee");
-        layout = new GridLayout();
+        layout = new GridLayout(3,3);
     }
 
     public static void main(String args[]) {
@@ -30,9 +32,13 @@ public class Healthee {
     }
 
     public void show() {
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(1000,800);
-        window.setLayout(layout);
-        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Pressing the windows X will terminate the program
+        window.setSize(dim.width/4*3,dim.height/4*3); //Size of frame is 3/4 of screen resolution for width and height
+        window.setLayout(layout); // Layout is 3x3 grid layout. Some grid spaces will be merged
+        window.add(new JButton("Test"));
+
+
+        window.setLocationRelativeTo(null); //Sets the window to appear in the center of the main monitor
+        window.setVisible(true); //Makes the frame visible
     }
 }
