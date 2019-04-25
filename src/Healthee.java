311b1941d4bcc9e3df828d40dfac1434b11bd86a
Alexander Schacht <a.schacht24@gmail.com>
2019-04-25 15:22:57 +0200

    Set up the classes I think is needed. Also added initial GUI elements to main class "Healthee" which will handle the GUI. The other classes will handle data representation and calculations/formats.

--- src/Healthee.java
+++ src/Healthee.java
@@ -1,2 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Healthee {
+
+    //MODULES
+    private Settings settings;
+    private SpecificEmployee specificEmployee;
+    private Timeframe timeframe;
+    private TotalIllnessHistory totalIllnessHistory;
+
+    //GUI ELEMENTS
+    private JFrame window;
+    private LayoutManager layout;
+
+
+    private Healthee() {
+        settings = new Settings();
+        specificEmployee = new SpecificEmployee();
+        timeframe = new Timeframe();
+        totalIllnessHistory = new TotalIllnessHistory();
+
+        window = new JFrame("Healthee");
+        layout = new GridLayout();
+    }
+
+    public static void main(String args[]) {
+        Healthee program = new Healthee();
+        program.show();
+    }
+
+    public void show() {
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(1000,800);
+        window.setLayout(layout);
+        window.setVisible(true);
+    }
 }
