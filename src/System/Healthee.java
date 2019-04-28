168d0dbd3293b279fb97270ebc3f5c4ee4af2bf4
Alexander Schacht <a.schacht24@gmail.com>
2019-04-28 12:24:06 +0200

    Defined packages for system code, and code to replicate employee sickness data. Also started setting up the frame to contain different content panes for the different displays.

--- src/System/Healthee.java
+++ src/System/Healthee.java
@@ -1,3 +1,5 @@
+package System;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -13,17 +15,40 @@ public class Healthee {
 
     //GUI ELEMENTS
     private JFrame window;
-    private LayoutManager layout;
 
+    //GUI WINDOWS
+    private Container mainContainer;
+    private Container settingsContainer;
+    private Container specificEmployeeContainer;
+    private Container totalIllnessHistoryContainer;
+
+    //GUI LAYOUTS
+    private LayoutManager mainLayout;
+    private LayoutManager settingsLayout;
+    private LayoutManager specificEmployeeLayout;
+    private LayoutManager totalIllnessHistoryLayout;
 
     private Healthee() {
+        //INIT SYSTEMS
         settings = new Settings();
         specificEmployee = new SpecificEmployee();
         timeframe = new Timeframe();
         totalIllnessHistory = new TotalIllnessHistory();
 
+        //INIT GUI ELEMENTS
         window = new JFrame("Healthee");
-        layout = new GridLayout(3,3);
+        mainContainer = new Container();
+        mainLayout = new GridLayout(3,3);
+
+        settingsContainer = new Container();
+        settingsLayout = new GridLayout(3,3);
+
+        specificEmployeeContainer = new Container();
+        specificEmployeeLayout = new GridLayout(3,3);
+
+        totalIllnessHistoryContainer = new Container();
+        totalIllnessHistoryLayout = new GridLayout(3,3);
+
     }
 
     public static void main(String args[]) {
@@ -34,9 +59,13 @@ public class Healthee {
     public void show() {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Pressing the windows X will terminate the program
         window.setSize(dim.width/4*3,dim.height/4*3); //Size of frame is 3/4 of screen resolution for width and height
-        window.setLayout(layout); // Layout is 3x3 grid layout. Some grid spaces will be merged
-        window.add(new JButton("Test"));
+        setupMainScreen();
+    }
 
+    public void setupMainScreen() {
+        //window.setLayout(mainLayout);
+        window.setContentPane(mainContainer);
+        mainContainer.setLayout(mainLayout); // Layout is 3x3 grid layout. Some grid spaces will be merged
 
         window.setLocationRelativeTo(null); //Sets the window to appear in the center of the main monitor
         window.setVisible(true); //Makes the frame visible
--- src/System/TotalIllnessHistory.java
+++ src/System/TotalIllnessHistory.java
@@ -1,2 +1,4 @@
+package System;
+
 public class TotalIllnessHistory {
 }
