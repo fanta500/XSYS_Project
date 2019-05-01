508f8932557f6b13da9107c86222edc526645376
Alexander Schacht <a.schacht24@gmail.com>
2019-05-01 12:16:54 +0200

    Big changes.

--- src/System/Healthee.java
+++ src/System/Healthee.java
@@ -8,7 +8,9 @@ import java.awt.event.ActionListener;
 
 public class Healthee {
     //MISC
-    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    private double sideBarMenuWidth = dim.getWidth()/20;
+    private String lastView = "";
 
     //MODULES
     private Settings settings;
@@ -21,6 +23,8 @@ public class Healthee {
 
     //GUI PANELS
     private JPanel mainPanel;
+    private JPanel mainCenterPanel;
+
     private JPanel settingsPanel;
     private JPanel specificEmployeePanel;
     private JPanel totalIllnessHistoryPanel;
@@ -29,6 +33,8 @@ public class Healthee {
 
     //GUI LAYOUTS
     private LayoutManager mainLayout;
+    private LayoutManager mainCenterLayout;
+
     private LayoutManager settingsLayout;
     private LayoutManager specificEmployeeLayout;
     private LayoutManager totalIllnessHistoryLayout;
@@ -37,8 +43,15 @@ public class Healthee {
 
     //GUI ELEMENTS
     private JButton speceficEmplyoeeButton;
+    private JButton totalIllnessHustoryButton;
+    private JButton illTodayButton;
+    private JButton aggIllnessDaysButton;
+
     private JButton homeButton;
-    private ActionListener homeButtonPressed;
+    private JButton returnButton;
+
+    private JList<DefaultListModel> timeFrameList;
+    private DefaultListModel timeFrameListModel;
 
     //GUI GRAPHICS
     private ImageIcon homeButtonIcon;
@@ -53,58 +66,103 @@ public class Healthee {
 
         //INIT GUI ELEMENTS
         window = new JFrame("Healthee");
+
+        //MAIN MENU
         mainLayout = new BorderLayout();
+        mainCenterLayout = new GridLayout(2,2,5,5); //2x2 grid for the 4 main view action options
+        mainCenterPanel = new JPanel(mainCenterLayout);
         mainPanel = new JPanel(mainLayout);
 
+        //SETTINGS
         settingsLayout = new BorderLayout();
         settingsPanel = new JPanel(settingsLayout);
 
+        //SPECIFIC EMPLOYEE
         specificEmployeeLayout = new BorderLayout();
         specificEmployeePanel = new JPanel(specificEmployeeLayout);
 
+        //TOTAL ILLNESS HISTORY
         totalIllnessHistoryLayout = new BorderLayout();
         totalIllnessHistoryPanel = new JPanel(totalIllnessHistoryLayout);
 
-        sideBarMenuLayout = new GridLayout(10,1);
+        //BUTTONS
+        speceficEmplyoeeButton = new JButton("Specific Employee");
+        speceficEmplyoeeButton.setBackground(Color.white);
+        illTodayButton = new JButton("Ill Today");
+        illTodayButton.setBackground(Color.white);
+        totalIllnessHustoryButton = new JButton("Total Illness History");
+        totalIllnessHustoryButton.setBackground(Color.white);
+        aggIllnessDaysButton = new JButton("Agg. Illness Days");
+        aggIllnessDaysButton.setBackground(Color.white);
+
+        Dimension sideBarMenuButtonDim = new Dimension((int)sideBarMenuWidth,(int)sideBarMenuWidth);
+        homeButton = new JButton("Home");
+        homeButton.setBackground(Color.white);
+        homeButton.setPreferredSize(sideBarMenuButtonDim);
+
+        returnButton = new JButton("Return to \n"+lastView);
+        returnButton.setBackground(Color.white);
+        returnButton.setPreferredSize(sideBarMenuButtonDim);
+
+        //TIME FRAME LIST SETUP
+        timeFrameListModel = new DefaultListModel<>();
+        timeFrameListModel.addElement("Past Week");
+        timeFrameListModel.addElement("Past 2 Weeks");
+        timeFrameListModel.addElement("Past Month");
+        timeFrameListModel.addElement("Past 3 Months");
+        timeFrameListModel.addElement("Past 6 Months");
+        timeFrameListModel.addElement("Past Year");
+        timeFrameListModel.addElement("All Time");
+
+        timeFrameList = new JList<>(timeFrameListModel);
+        //timeFrameList.setPreferredSize(new Dimension(100,125));
+        timeFrameList.setFixedCellWidth((int)sideBarMenuWidth);
+        timeFrameList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        timeFrameList.setLayoutOrientation(JList.VERTICAL);
+
+        //SIDEBAR
+        GridBagConstraints sideBarMenuConstraints = new GridBagConstraints();
+        sideBarMenuConstraints.gridx = 0;
+        sideBarMenuConstraints.gridy = 0;
+        sideBarMenuConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        sideBarMenuLayout = new GridBagLayout();
         sideBarMenu = new JPanel(sideBarMenuLayout);
+        sideBarMenu.add(homeButton, sideBarMenuConstraints);
+        sideBarMenuConstraints.gridy = 1;
+        JTextField timeFrameText = new JTextField("Time Frame");
+        timeFrameText.setPreferredSize(new Dimension((int)sideBarMenuWidth, 25));
+        timeFrameText.setEditable(false);
+        sideBarMenu.add(timeFrameText, sideBarMenuConstraints);
+        sideBarMenuConstraints.gridy = 2;
+        sideBarMenu.add(timeFrameList, sideBarMenuConstraints);
 
-        speceficEmplyoeeButton = new JButton("Lars Larsen");
-        homeButton = new JButton();
-
-        try {
-            Image img = ImageIO.read(getClass().getResource("resources/home_button.png"));
-            homeButton.setIcon(new ImageIcon(img));
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
 
         //Sidebar menu setup
-        homeButtonPressed = new ActionListener() {
+        homeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Home Button pressed");
                 openMainScreen();
-            }
-        };
-        homeButton.addActionListener(homeButtonPressed);
-
-        //Setting up default settings for different views
-        //Main view
-        mainPanel.setLayout(mainLayout); // Layout is a borderLayout
-        mainPanel.add(sideBarMenu, BorderLayout.WEST);
-        mainPanel.add(speceficEmplyoeeButton,BorderLayout.CENTER);
-        speceficEmplyoeeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Specific Employee Button pressed");
-                openSpecificEmployee();
             }
         });
 
-        //Specific employee view
-        specificEmployeePanel.add(sideBarMenu,BorderLayout.WEST);
-        sideBarMenu.add(homeButton);
-        window.setContentPane(specificEmployeePanel);
+        //Setting up default settings for different views
+        //Main view
+        mainPanel.add(sideBarMenu, BorderLayout.WEST);
+        mainPanel.add(mainCenterPanel, BorderLayout.CENTER);
+        mainCenterPanel.add(illTodayButton);
+        mainCenterPanel.add(speceficEmplyoeeButton);
+        mainCenterPanel.add(totalIllnessHustoryButton);
+        mainCenterPanel.add(aggIllnessDaysButton);
+        speceficEmplyoeeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: This has to not be hardcoded, but instead use a proper object reference later
+                lastView = "Specific Employee";
+                //returnButton.setText("Return to \n"+lastView);
+                returnButton.setText("<html><center>"+"Return to"+"<br>"+lastView+"</center></html>");
+                openSpecificEmployee();
+            }
+        });
     }
 
     public static void main(String args[]) {
@@ -116,6 +174,7 @@ public class Healthee {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Pressing the windows X will terminate the program
         window.setSize(dim.width/4*3,dim.height/4*3); //Size of frame is 3/4 of screen resolution for width and height
         openMainScreen();
+        window.setResizable(false);
         window.setContentPane(mainPanel);
         window.setLocationRelativeTo(null); //Sets the window to appear in the center of the main monitor
         window.setVisible(true); //Makes the frame visible
@@ -125,12 +184,18 @@ public class Healthee {
         specificEmployeePanel.setVisible(false);
         settingsPanel.setVisible(false);
         totalIllnessHistoryPanel.setVisible(false);
+        sideBarMenu.remove(homeButton);
+        sideBarMenu.add(returnButton);
+        mainPanel.add(sideBarMenu,BorderLayout.WEST);
         window.setContentPane(mainPanel);
         mainPanel.setVisible(true);
     }
 
     public void openSpecificEmployee() {
         mainPanel.setVisible(false);
+        sideBarMenu.remove(returnButton);
+        sideBarMenu.add(homeButton);
+        specificEmployeePanel.add(sideBarMenu,BorderLayout.WEST);
         window.setContentPane(specificEmployeePanel);
         specificEmployeePanel.setVisible(true);
     }
